cd7bf1bfae27ce5784aa450bc3e986f1eef2d218
opsb <oliver@opsb.co.uk>
2012-06-04 10:56:33 +0200

    Add rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alejandraaviles::Application.config.secret_token = 'b4723110ee49a8712a6a06e196c50b86ed9b3a62f12eb9410a17347c92ed196f888dd0971edbaa46aa1cd462da9ed075cf7acdc9a922ffc8af6f22eaf6a36ac7'
