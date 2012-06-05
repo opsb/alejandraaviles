560f762d2d15a86a7e4271861870caaa72b6fa79
opsb <oliver@opsb.co.uk>
2012-06-05 19:25:20 +0200

    wip - constructing layout

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Alejandraaviles::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
+  resources :pages
+  root :to => "pages#home"
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
